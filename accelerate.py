17cab7e6c1a8b7680d0506f01dcba2e8ce30438c
Marcus Lassila <marcus@ducce.net>
2025-07-23 01:06:00 +0200

    Adding AcceleratorLite for distributed training

---
+++ accelerate.py
@@ -0,0 +1,67 @@
+import torch
+import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.utils.data import DataLoader
+from torch.utils.data.distributed import DistributedSampler
+
+import os
+
+class AcceleratorLite:
+    '''Lightweight Accelerator (Huggingface) like class to handle device placement and distributed training.'''
+
+    def __init__(self, batch_size):
+        self.batch_size = batch_size
+        self.running_ddp = "RANK" in os.environ
+        if self.running_ddp:
+            assert torch.cuda.is_available()
+            dist.init_process_group(backend="nccl")
+            self.rank = int(os.environ["RANK"])
+            self.local_rank = int(os.environ["LOCAL_RANK"])
+            self.world_size = int(os.environ["WORLD_SIZE"])
+            self.device = torch.device(f"cuda:{self.local_rank}")
+            torch.cuda.set_device(self.device)
+        else:
+            self.rank = 0
+            self.local_rank = 0
+            self.world_size = 1
+            self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.is_master_process = self.rank == 0
+        self.device_type = "cuda" if torch.cuda.is_available() else "cpu"
+
+    def __del__(self):
+        if self.running_ddp:
+            self.print("destroying process group")
+            dist.destroy_process_group()
+
+    def _get_dataloader(self, dataset):
+        if self.running_ddp:
+            train_sampler = DistributedSampler(dataset, num_replicas=self.world_size, rank=self.rank)
+            dataloader = DataLoader(dataset, batch_size=self.batch_size, sampler=train_sampler)
+        else:
+            dataloader = DataLoader(dataset, batch_size=self.batch_size, shuffle=True)
+        return DataLoaderOnDevice(dataloader, self.device)
+
+    def prepare(self, model, train_dataset, val_dataset):
+        model.to(self.device)
+        if torch.cuda.is_available():
+            model = torch.compile(model)
+        if self.running_ddp:
+            model = DDP(model, device_ids=[self.local_rank])
+        train_dataloader = self._get_dataloader(train_dataset)
+        val_dataloader = self._get_dataloader(val_dataset)
+        return model, train_dataloader, val_dataloader
+
+    def print(self, *args, **kwargs):
+        if self.is_master_process:
+            print(*args, **kwargs)
+
+class DataLoaderOnDevice:
+    '''Wrapper to place batches on device.'''
+
+    def __init__(self, dataloader, device):
+        self.dataloader = dataloader
+        self.device = device
+
+    def __iter__(self):
+        for batch in self.dataloader:
+            yield batch.to(self.device)
--- ddpm.py
+++ ddpm.py
@@ -1,11 +1,14 @@
+import accelerate
 import data
-import model
+import models
 import utils
 
 import numpy as np
 import torch
+import torch.distributed as dist
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
+from tqdm.auto import tqdm
 
 import time
 from pathlib import Path
@@ -13,12 +16,11 @@ from pathlib import Path
 class DDPM:
 
     def __init__(self, beta, train_dataset, val_dataset, batch_size, device):
-        self.train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-        self.val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+        self.accelerator = accelerate.AcceleratorLite(batch_size=batch_size)
         self.batch_size = batch_size
         self.input_dim = train_dataset[0].shape
         self.device = device
-        self.model = model.UNet(
+        self.model = models.UNet(
             size=32,
             in_channels=3,
             out_channels=3,
@@ -26,7 +28,7 @@ class DDPM:
             dropout=0.1,
             resample_with_conv=True,
         ) # Model that predict noise
-        self.model.to(device)
+        self.model, self.train_loader, self.val_loader = self.accelerator.prepare(self.model, train_dataset, val_dataset)
         self.beta = torch.tensor(beta).to(device)
         self.T = self.beta.shape[0]
         self.alpha_bar = torch.cumprod(1 - self.beta, dim=0)
@@ -51,10 +53,21 @@ class DDPM:
                 loss.backward()
                 norm = torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1.0)
                 self.optimizer.step()
+                loss = loss.detach()
                 step += 1
+                if self.accelerator.running_ddp:
+                    dist.all_reduce(loss, op=dist.ReduceOp.AVG)
+                if torch.cuda.is_available():
+                    torch.cuda.synchronize()
                 t1 = time.time()
-                im_per_sec = B / (t1 - t0)
-                print(f"step: {step} | train loss: {loss.item() / B:.2f} | grad norm: {norm:.2f} | images per sec: {im_per_sec:.1f}", flush=True)
+                im_per_sec = B * self.accelerator.world_size / (t1 - t0)
+                log_msg = (
+                    f"step: {step} "
+                    f"| train loss: {loss.item() / B:.2f} "
+                    f"| grad norm: {norm:.2f} "
+                    f"| images per sec: {im_per_sec:.1f}"
+                )
+                self.accelerator.print(log_msg, flush=True)
                 t0 = time.time()
 
             self.model.eval()
@@ -71,22 +84,24 @@ class DDPM:
                     loss = F.mse_loss(input=noise_pred, target=eps, reduction="sum")
                     val_loss += loss.item() / B
                 val_loss /= len(self.val_loader)
-                print(f"epoch: {epoch} | val loss: {val_loss:.2f}", flush=True)
+                if self.accelerator.running_ddp:
+                    dist.all_reduce(val_loss, op=dist.ReduceOp.AVG)
+                self.accelerator.print(f"epoch: {epoch} | val loss: {val_loss:.2f}", flush=True)
                 if val_loss < best_val_loss:
                     best_val_loss = val_loss
-                    savepath = f"./models/{self.train_loader.dataset.__class__.__name__}_model.pth"
-                    Path("./models").mkdir(parents=True, exist_ok=True)
+                    savepath = f"./trained_models/{self.train_loader.dataset.__class__.__name__}_model.pth"
+                    Path("./trained_models").mkdir(parents=True, exist_ok=True)
                     torch.save(self.model.state_dict(), savepath)
 
     def load(self, model_name):
-        self.model.load_state_dict(torch.load(model_name))
+        self.model.load_state_dict(torch.load(model_name, map_location=self.device))
         self.model.eval()
 
     def sample(self):
         self.model.eval()
         with torch.no_grad():
             x = torch.randn(self.batch_size, *self.input_dim)
-            for t in range(self.T - 1, -1, -1):
+            for t in tqdm(range(self.T - 1, -1, -1), desc=f"Sampling {self.batch_size} images"):
                 t = torch.tensor([t]).to(self.device)
                 if t > 0:
                     z = torch.randn(self.batch_size, *self.input_dim)
@@ -98,19 +113,22 @@ class DDPM:
         return x
 
 if __name__ == "__main__":
+    torch.set_float32_matmul_precision('high')
     train_dataset = data.CIFAR10(train=True)
     val_dataset = data.CIFAR10(train=False)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    batch_size = 128
     ddpm = DDPM(
         beta=np.linspace(start=1e-4, stop=0.02, num=1000, dtype=np.float32),
         train_dataset=train_dataset,
         val_dataset=val_dataset,
-        batch_size=128,
+        batch_size=batch_size,
         device=device,
     )
     ddpm.train(10)
-    # ddpm.load("models/CIFAR10_final_model.pth")
-    # x = next(iter(train_dataset))
-    # utils.plot_image(x)
-    # x = ddpm.sample()
-    # utils.plot_image(x[0])
+    # ddpm.load("./trained_models/CIFAR10_model.pth")
+    x = next(iter(train_dataset))
+    utils.plot_image(x)
+    x = ddpm.sample()
+    for i in range(batch_size):
+        utils.plot_image(x[i])
