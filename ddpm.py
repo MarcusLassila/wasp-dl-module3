555206187b6d8b9ec5d043ea4c8ee10cf50882d2
Marcus Lassila <marcus@ducce.net>
2025-07-24 18:07:49 +0200

    Use gradient accumulation to simulate larger batch size and optionally save raw generated image data

--- ddpm.py
+++ ddpm.py
@@ -49,6 +49,10 @@ class DDPM:
         input_dim = train_dataset[0].shape
         assert input_dim == self.image_dim
 
+        simulated_batch_size = 64
+        assert simulated_batch_size % batch_size == 0
+        grad_accum_steps = simulated_batch_size // batch_size
+
         accelerator = accelerate.AcceleratorLite(batch_size=batch_size, do_compile=False)
         if torch.device(self.device).type == "cuda" and torch.cuda.is_available():
             autocast_context = torch.autocast(device_type="cuda", dtype=torch.bfloat16)
@@ -67,31 +71,41 @@ class DDPM:
         best_val_loss = float('inf')
         for epoch in range(1, n_epochs + 1):
             model.train()
+            optimizer.zero_grad()
+            accum_train_loss = 0.0
             t0 = time.time()
-            for x in train_loader:
+            for i, x in enumerate(train_loader):
+                final_grad_accum_step = (i + 1) % grad_accum_steps == 0
                 B = x.shape[0]
                 t = torch.randint(low=0, high=self.T, size=(B,)).to(self.device)
                 eps = torch.randn(x.shape).to(self.device)
                 alpha_bar_t = self.alpha_bar[t].view(B, 1, 1, 1)
                 z = torch.sqrt(alpha_bar_t) * x + torch.sqrt(1 - alpha_bar_t) * eps
-                optimizer.zero_grad()
                 with autocast_context:
                     noise_pred = model(z, t)
                     loss = F.mse_loss(input=noise_pred, target=eps, reduction="mean")
+                loss /= grad_accum_steps
+                accum_train_loss += loss.detach()
+                if accelerator.running_ddp:
+                    model.require_backward_grad_sync = (i + 1) % final_grad_accum_step
                 loss.backward()
+                if not final_grad_accum_step:
+                    continue # Keep accumulating gradients
+
+                # Gradient accumulation done, update parameters and log
+                if accelerator.running_ddp:
+                    dist.all_reduce(accum_train_loss, op=dist.ReduceOp.AVG)
                 norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
                 optimizer.step()
-                loss = loss.detach()
-                step += 1
-                if accelerator.running_ddp:
-                    dist.all_reduce(loss, op=dist.ReduceOp.AVG)
+                optimizer.zero_grad()
                 if torch.cuda.is_available():
-                    torch.cuda.synchronize()
+                    torch.cuda.synchronize() # Syncronize processes before measuring t1
                 t1 = time.time()
+                step += 1
                 im_per_sec = B * accelerator.world_size / (t1 - t0)
                 log_msg = (
                     f"step: {step} "
-                    f"| train loss: {loss.item():.6f} "
+                    f"| train loss: {accum_train_loss.item():.6f} "
                     f"| grad norm: {norm:.2f} "
                     f"| images per sec: {im_per_sec:.1f} "
                     f"| dt: {t1 - t0:.2f}"
--- infer_celeba_hq.py
+++ infer_celeba_hq.py
@@ -3,32 +3,30 @@ import utils
 
 import torch
 
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--save_raw_data", action="store_true")
+args = parser.parse_args()
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# checkpoint = torch.load("./trained_models/CelebAHQ_model.pth", map_location=device)
-# ddpm_obj = ddpm.DDPM(
-#     beta=checkpoint["beta"],
-#     channel_mult=checkpoint["channel_mult"],
-#     image_dim=checkpoint["image_dim"],
-#     device=device,
-#     base_channels=checkpoint["base_channels"],
-#     dropout=checkpoint["dropout"],
-#     resample_with_conv=checkpoint["resample_with_conv"],
-# )
 checkpoint = torch.load("./trained_models/CelebAHQ_model.pth", map_location=device)
 ddpm_obj = ddpm.DDPM(
-    beta=torch.linspace(start=1e-4, end=0.02, steps=1000),
-    channel_mult=(1, 1, 2, 2, 4, 4),
-    image_dim=(3,256,256),
+    beta=checkpoint["beta"],
+    channel_mult=checkpoint["channel_mult"],
+    image_dim=checkpoint["image_dim"],
     device=device,
-    base_channels=128,
-    dropout=0.0,
-    resample_with_conv=True,
+    base_channels=checkpoint["base_channels"],
+    dropout=checkpoint["dropout"],
+    resample_with_conv=checkpoint["resample_with_conv"],
 )
-# ddpm_obj.load(checkpoint["state_dict"])
-ddpm_obj.load(checkpoint)
-batch_size = 1
+ddpm_obj.load(checkpoint["state_dict"])
+batch_size = 16
 gen_batch = ddpm_obj.sample(batch_size)
-for i in range(batch_size):
-    utils.plot_image(gen_batch[i], rescale_method="clamp", name=f"clamped_{i}")
-for i in range(batch_size):
-    utils.plot_image(gen_batch[i], rescale_method="tanh", name=f"tanhed_{i}")
+if args.save_raw_data:
+    torch.save(gen_batch, "./images/image_batch.pth")
+else:
+    for i in range(batch_size):
+        utils.plot_image(gen_batch[i], rescale_method="clamp", name=f"clamped_{i}")
+    for i in range(batch_size):
+        utils.plot_image(gen_batch[i], rescale_method="tanh", name=f"tanhed_{i}")
