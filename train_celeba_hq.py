37c1918ec20d59cfbaeb405a8dfbc11a7402f304
Marcus Lassila <lassila@alvis1.int.private>
2025-07-24 22:51:24 +0200

    Fix arguments

--- train_celeba_hq.py
+++ train_celeba_hq.py
@@ -7,18 +7,15 @@ torch.set_float32_matmul_precision('high')
 train_dataset = data.CelebAHQ(train=True)
 val_dataset = data.CelebAHQ(train=False)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-batch_size = 16
 beta = torch.linspace(start=1e-4, end=0.02, steps=1000)
 channel_mult = (1, 1, 2, 2, 4, 4)
-n_epochs = 10
+image_dim = train_dataset[0].shape
 ddpm.DDPM(
     beta=beta,
     channel_mult=channel_mult,
-    train_dataset=train_dataset,
-    val_dataset=val_dataset,
-    batch_size=batch_size,
+    image_dim=image_dim,
     device=device,
     base_channels=128,
     dropout=0.0,
-    lr=2e-5,
-).train(n_epochs)
+    resample_with_conv=True,
+).train(train_dataset, val_dataset, batch_size=2, lr=2e-5, n_epochs=3)
