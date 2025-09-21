995af118feebfc46d47ae003cc5698c856f1e09b
Marcus Lassila <marcus@ducce.net>
2025-09-21 13:35:03 +0200

    Add pyproject.toml, CIFAR-10.1 for FID computation, and update conda env files.

--- ddpm/data.py
+++ ddpm/data.py
@@ -1,11 +1,13 @@
+import numpy as np
 import torch
+import tensorflow_datasets as tfds
 from datasets import load_dataset
 from torchvision import datasets
 from torchvision import transforms as T
-from torch.utils.data import ConcatDataset, DataLoader, Dataset
+from torch.utils.data import ConcatDataset, DataLoader, Dataset, TensorDataset
 
 class CIFAR10(Dataset):
-    
+
     def __init__(self):
         self.transform = T.Compose([
             T.RandomHorizontalFlip(p=0.5),
@@ -16,11 +18,29 @@ class CIFAR10(Dataset):
             datasets.CIFAR10(root='./data', train=True, download=True, transform=self.transform),
             datasets.CIFAR10(root='./data', train=False, download=True, transform=self.transform),
         ])
-    
+
     def __getitem__(self, index):
         item, _ = self.dataset[index] # Discard labels
         return item
+
+    def __len__(self):
+        return len(self.dataset)
     
+class CIFAR10_1(Dataset):
+
+    def __init__(self, transform=None):
+        # Load CIFAR-10.1 v6 (~2,000 images)
+        ds = tfds.load("cifar10_1/v6", split="test", as_supervised=True)
+
+        self.dataset = torch.from_numpy(np.array([x for x, _ in tfds.as_numpy(ds)])).permute(0, 3, 1, 2)  # (N,3,32,32)
+        self.transform = transform
+
+    def __getitem__(self, index):
+        item = self.dataset[index]
+        if self.transform is not None:
+            item = self.transform(item)
+        return item
+
     def __len__(self):
         return len(self.dataset)
 
@@ -66,12 +86,8 @@ class Flowers(Dataset):
     def __len__(self):
         return len(self.dataset)
 
-def inverse_transform(tensors):
-    """Convert tensors from [-1., 1.] to [0., 255.]"""
-    return ((tensors.clamp(-1, 1) + 1.0) / 2.0) * 255.0
-
 if __name__ == "__main__":
-    dataset = CIFAR10()
+    dataset = CIFAR10_1()
     print(len(dataset))
     import utils
     img = dataset[1]
