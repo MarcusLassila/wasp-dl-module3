1dbc09c3692a9b0f89c056bdd1a00aae91c4d50f
Marcus Lassila <lassila@chalmers.se>
2025-09-29 16:54:58 +0200

    Update setup file to set pythonpath

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+# Prepend repo root (current dir) to PYTHONPATH if not already present
+if [[ ":${PYTHONPATH:-}:" != *":$PWD:"* ]]; then
+  export PYTHONPATH="$PWD${PYTHONPATH:+:$PYTHONPATH}"
+fi
+
+echo "PYTHONPATH=$PYTHONPATH"
 pip install -e .
